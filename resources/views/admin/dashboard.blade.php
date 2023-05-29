6386560fbffc447f8751ce4fd070c5d78c05191f
Oecophyllaa <rangga.godzila@gmail.com>
2023-05-29 20:44:40 +0700

    admin login & dashboard

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,315 @@
+<x-admin-layout>
+  <!-- cards -->
+  <div class="mx-auto w-full px-6 py-6">
+    <!-- row 1 -->
+    <div class="-mx-3 flex flex-wrap">
+      <!-- card1 -->
+      <div class="mb-6 w-full max-w-full px-3 sm:w-1/2 sm:flex-none xl:mb-0 xl:w-1/4">
+        <div class="dark:bg-slate-850 dark:shadow-dark-xl relative flex min-w-0 flex-col break-words rounded-2xl bg-white bg-clip-border shadow-xl">
+          <div class="flex-auto p-4">
+            <div class="-mx-3 flex flex-row">
+              <div class="w-2/3 max-w-full flex-none px-3">
+                <div>
+                  <p class="mb-0 font-sans text-sm font-semibold uppercase leading-normal dark:text-white dark:opacity-60">
+                    Total Layanan</p>
+                  <h5 class="mb-2 font-bold dark:text-white">10</h5>
+                </div>
+              </div>
+              <div class="basis-1/3 px-3 text-right">
+                <div class="rounded-circle inline-block h-12 w-12 bg-gradient-to-tl from-blue-500 to-violet-500 text-center">
+                  <i class="ni ni-money-coins relative top-3.5 text-lg leading-none text-white"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- card2 -->
+      <div class="mb-6 w-full max-w-full px-3 sm:w-1/2 sm:flex-none xl:mb-0 xl:w-1/4">
+        <div class="dark:bg-slate-850 dark:shadow-dark-xl relative flex min-w-0 flex-col break-words rounded-2xl bg-white bg-clip-border shadow-xl">
+          <div class="flex-auto p-4">
+            <div class="-mx-3 flex flex-row">
+              <div class="w-2/3 max-w-full flex-none px-3">
+                <div>
+                  <p class="mb-0 font-sans text-sm font-semibold uppercase leading-normal dark:text-white dark:opacity-60">
+                    Order Masuk</p>
+                  <h5 class="mb-2 font-bold dark:text-white">3</h5>
+                </div>
+              </div>
+              <div class="basis-1/3 px-3 text-right">
+                <div class="rounded-circle inline-block h-12 w-12 bg-gradient-to-tl from-red-600 to-orange-600 text-center">
+                  <i class="ni ni-delivery-fast relative top-3.5 text-lg leading-none text-white"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- card3 -->
+      <div class="w-full max-w-full px-3 sm:w-1/2 sm:flex-none xl:w-1/4">
+        <div class="dark:bg-slate-850 dark:shadow-dark-xl relative flex min-w-0 flex-col break-words rounded-2xl bg-white bg-clip-border shadow-xl">
+          <div class="flex-auto p-4">
+            <div class="-mx-3 flex flex-row">
+              <div class="w-2/3 max-w-full flex-none px-3">
+                <div>
+                  <p class="mb-0 font-sans text-sm font-semibold uppercase leading-normal dark:text-white dark:opacity-60">
+                    Jumlah Projects</p>
+                  <h5 class="mb-2 font-bold dark:text-white">10</h5>
+                </div>
+              </div>
+              <div class="basis-1/3 px-3 text-right">
+                <div class="rounded-circle inline-block h-12 w-12 bg-gradient-to-tl from-emerald-500 to-teal-400 text-center">
+                  <i class="ni ni-app relative top-3.5 text-lg leading-none text-white"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- cards row 3 -->
+
+    <div class="-mx-3 mt-6 flex flex-wrap">
+      <div class="mt-0 mb-6 w-full max-w-full px-3 lg:mb-0 lg:w-7/12 lg:flex-none">
+        <div
+          class="dark:bg-slate-850 dark:shadow-dark-xl dark:bg-gray-950 border-black-125 relative flex min-w-0 flex-col break-words rounded-2xl border-0 border-solid bg-white bg-clip-border shadow-xl">
+          <div class="rounded-t-4 mb-0 p-4 pb-0">
+            <div class="flex justify-between">
+              <h6 class="mb-2 dark:text-white">Pemesanan Terbaru</h6>
+            </div>
+          </div>
+          <div class="overflow-x-auto">
+            <table class="mb-4 w-full border-collapse items-center border-gray-200 align-top dark:border-white/40">
+              <tbody>
+                <tr>
+                  <td class="w-3/10 whitespace-nowrap border-b bg-transparent p-2 align-middle dark:border-white/40">
+                    <div class="flex items-center px-2 py-1">
+                      <div class="ml-6">
+                        <p class="mb-0 text-xs font-semibold leading-tight dark:text-white dark:opacity-60">Nama
+                          Pemesan:
+                        </p>
+                        <h6 class="mb-0 text-sm leading-normal dark:text-white">Fachry</h6>
+                      </div>
+                    </div>
+                  </td>
+                  <td class="whitespace-nowrap border-b bg-transparent p-2 align-middle dark:border-white/40">
+                    <div class="text-center">
+                      <p class="mb-0 text-xs font-semibold leading-tight dark:text-white dark:opacity-60">Layanan:</p>
+                      <h6 class="mb-0 text-sm leading-normal dark:text-white">Annual Report</h6>
+                    </div>
+                  </td>
+                  <td class="whitespace-nowrap border-b bg-transparent p-2 align-middle dark:border-white/40">
+                    <div class="text-center">
+                      <p class="mb-0 text-xs font-semibold leading-tight dark:text-white dark:opacity-60">Kategori:
+                      </p>
+                      <h6 class="mb-0 text-sm leading-normal dark:text-white">Report Management</h6>
+                    </div>
+                  </td>
+                  <td class="whitespace-nowrap border-b bg-transparent p-2 align-middle text-sm leading-normal dark:border-white/40">
+                    <div class="flex-1 text-center">
+                      <p class="mb-0 text-xs font-semibold leading-tight dark:text-white dark:opacity-60">Tanggal:</p>
+                      <h6 class="mb-0 text-sm leading-normal dark:text-white">29/04/23</h6>
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td class="w-3/10 whitespace-nowrap border-b bg-transparent p-2 align-middle dark:border-white/40">
+                    <div class="flex items-center px-2 py-1">
+                      <div class="ml-6">
+                        <p class="mb-0 text-xs font-semibold leading-tight dark:text-white dark:opacity-60">Nama
+                          Pemesan:
+                        </p>
+                        <h6 class="mb-0 text-sm leading-normal dark:text-white">Fachry</h6>
+                      </div>
+                    </div>
+                  </td>
+                  <td class="whitespace-nowrap border-b bg-transparent p-2 align-middle dark:border-white/40">
+                    <div class="text-center">
+                      <p class="mb-0 text-xs font-semibold leading-tight dark:text-white dark:opacity-60">Layanan:</p>
+                      <h6 class="mb-0 text-sm leading-normal dark:text-white">Annual Report</h6>
+                    </div>
+                  </td>
+                  <td class="whitespace-nowrap border-b bg-transparent p-2 align-middle dark:border-white/40">
+                    <div class="text-center">
+                      <p class="mb-0 text-xs font-semibold leading-tight dark:text-white dark:opacity-60">Kategori:
+                      </p>
+                      <h6 class="mb-0 text-sm leading-normal dark:text-white">Report Management</h6>
+                    </div>
+                  </td>
+                  <td class="whitespace-nowrap border-b bg-transparent p-2 align-middle text-sm leading-normal dark:border-white/40">
+                    <div class="flex-1 text-center">
+                      <p class="mb-0 text-xs font-semibold leading-tight dark:text-white dark:opacity-60">Tanggal:</p>
+                      <h6 class="mb-0 text-sm leading-normal dark:text-white">29/04/23</h6>
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td class="w-3/10 whitespace-nowrap border-b bg-transparent p-2 align-middle dark:border-white/40">
+                    <div class="flex items-center px-2 py-1">
+                      <div class="ml-6">
+                        <p class="mb-0 text-xs font-semibold leading-tight dark:text-white dark:opacity-60">Nama
+                          Pemesan:
+                        </p>
+                        <h6 class="mb-0 text-sm leading-normal dark:text-white">Fachry</h6>
+                      </div>
+                    </div>
+                  </td>
+                  <td class="whitespace-nowrap border-b bg-transparent p-2 align-middle dark:border-white/40">
+                    <div class="text-center">
+                      <p class="mb-0 text-xs font-semibold leading-tight dark:text-white dark:opacity-60">Layanan:</p>
+                      <h6 class="mb-0 text-sm leading-normal dark:text-white">Annual Report</h6>
+                    </div>
+                  </td>
+                  <td class="whitespace-nowrap border-b bg-transparent p-2 align-middle dark:border-white/40">
+                    <div class="text-center">
+                      <p class="mb-0 text-xs font-semibold leading-tight dark:text-white dark:opacity-60">Kategori:
+                      </p>
+                      <h6 class="mb-0 text-sm leading-normal dark:text-white">Report Management</h6>
+                    </div>
+                  </td>
+                  <td class="whitespace-nowrap border-b bg-transparent p-2 align-middle text-sm leading-normal dark:border-white/40">
+                    <div class="flex-1 text-center">
+                      <p class="mb-0 text-xs font-semibold leading-tight dark:text-white dark:opacity-60">Tanggal:</p>
+                      <h6 class="mb-0 text-sm leading-normal dark:text-white">29/04/23</h6>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      <div class="mt-0 w-full max-w-full px-3 lg:w-5/12 lg:flex-none">
+        <div
+          class="border-black/12.5 dark:bg-slate-850 dark:shadow-dark-xl relative flex min-w-0 flex-col break-words rounded-2xl border-0 border-solid bg-white bg-clip-border shadow-xl">
+          <div class="rounded-t-4 p-4 pb-0">
+            <h6 class="mb-0 dark:text-white">Kategori</h6>
+          </div>
+          <div class="flex-auto p-4">
+            <ul class="mb-0 flex flex-col rounded-lg pl-0">
+              <li class="relative mb-2 flex justify-between rounded-xl rounded-t-lg border-0 py-2 pr-4 text-inherit">
+                <div class="flex items-center">
+                  <div
+                    class="dark:from-slate-750 dark:to-gray-850 mr-4 inline-block h-8 w-8 rounded-xl bg-gradient-to-tl from-zinc-800 to-zinc-700 bg-center fill-current stroke-none text-center text-black shadow-sm dark:bg-gradient-to-tl">
+                    <i class="ni ni-mobile-button top-0.75 text-xxs relative text-white"></i>
+                  </div>
+                  <div class="flex flex-col">
+                    <h6 class="mb-1 text-sm leading-normal text-slate-700 dark:text-white">Report Managements</h6>
+                    <span class="text-xs leading-tight dark:text-white/80">3 Layanan</span>
+                  </div>
+                </div>
+                <div class="flex">
+                  <button
+                    class="leading-pro rounded-3.5xl p-1.2 h-6.5 w-6.5 group mx-0 my-auto inline-block cursor-pointer border-0 bg-transparent text-center align-middle text-xs font-bold text-slate-700 shadow-none transition-all ease-in dark:text-white"><i
+                      class="ni ease-bounce text-2xs group-hover:translate-x-1.25 ni-bold-right transition-all duration-200"
+                      aria-hidden="true"></i></button>
+                </div>
+              </li>
+              <li class="relative mb-2 flex justify-between rounded-xl border-0 py-2 pr-4 text-inherit">
+                <div class="flex items-center">
+                  <div
+                    class="dark:from-slate-750 dark:to-gray-850 mr-4 inline-block h-8 w-8 rounded-xl bg-gradient-to-tl from-zinc-800 to-zinc-700 bg-center fill-current stroke-none text-center text-black shadow-sm dark:bg-gradient-to-tl">
+                    <i class="ni ni-tag top-0.75 text-xxs relative text-white"></i>
+                  </div>
+                  <div class="flex flex-col">
+                    <h6 class="mb-1 text-sm leading-normal text-slate-700 dark:text-white">Branding Service</h6>
+                    <span class="text-xs leading-tight dark:text-white/80">3 Layanan</span>
+                  </div>
+                </div>
+                <div class="flex">
+                  <button
+                    class="leading-pro rounded-3.5xl p-1.2 h-6.5 w-6.5 group mx-0 my-auto inline-block cursor-pointer border-0 bg-transparent text-center align-middle text-xs font-bold text-slate-700 shadow-none transition-all ease-in dark:text-white"><i
+                      class="ni ease-bounce text-2xs group-hover:translate-x-1.25 ni-bold-right transition-all duration-200"
+                      aria-hidden="true"></i></button>
+                </div>
+              </li>
+              <li class="relative mb-2 flex justify-between rounded-xl rounded-b-lg border-0 py-2 pr-4 text-inherit">
+                <div class="flex items-center">
+                  <div
+                    class="dark:from-slate-750 dark:to-gray-850 mr-4 inline-block h-8 w-8 rounded-xl bg-gradient-to-tl from-zinc-800 to-zinc-700 bg-center fill-current stroke-none text-center text-black shadow-sm dark:bg-gradient-to-tl">
+                    <i class="ni ni-box-2 top-0.75 text-xxs relative text-white"></i>
+                  </div>
+                  <div class="flex flex-col">
+                    <h6 class="mb-1 text-sm leading-normal text-slate-700 dark:text-white">Corporation</h6>
+                    <span class="text-xs leading-tight dark:text-white/80">4 Layanan</span>
+                  </div>
+                </div>
+                <div class="flex">
+                  <button
+                    class="leading-pro rounded-3.5xl p-1.2 h-6.5 w-6.5 group mx-0 my-auto inline-block cursor-pointer border-0 bg-transparent text-center align-middle text-xs font-bold text-slate-700 shadow-none transition-all ease-in dark:text-white"><i
+                      class="ni ease-bounce text-2xs group-hover:translate-x-1.25 ni-bold-right transition-all duration-200"
+                      aria-hidden="true"></i></button>
+                </div>
+              </li>
+              <li class="relative mb-2 flex justify-between rounded-xl rounded-b-lg border-0 py-2 pr-4 text-inherit">
+                <div class="flex items-center">
+                  <div
+                    class="dark:from-slate-750 dark:to-gray-850 mr-4 inline-block h-8 w-8 rounded-xl bg-gradient-to-tl from-zinc-800 to-zinc-700 bg-center fill-current stroke-none text-center text-black shadow-sm dark:bg-gradient-to-tl">
+                    <i class="ni ni-lock-circle-open top-0.75 text-xxs relative text-white"></i>
+                  </div>
+                  <div class="flex flex-col">
+                    <h6 class="mb-1 text-sm leading-normal text-slate-700 dark:text-white">Coming Soon</h6>
+                    <span class="text-xs leading-tight dark:text-white/80">2 Layanan</span>
+                  </div>
+                </div>
+                <div class="flex">
+                  <button
+                    class="leading-pro rounded-3.5xl p-1.2 h-6.5 w-6.5 group mx-0 my-auto inline-block cursor-pointer border-0 bg-transparent text-center align-middle text-xs font-bold text-slate-700 shadow-none transition-all ease-in dark:text-white"><i
+                      class="ni ease-bounce text-2xs group-hover:translate-x-1.25 ni-bold-right transition-all duration-200"
+                      aria-hidden="true"></i></button>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- <footer class="pt-4">
+        <div class="mx-auto w-full px-6">
+          <div class="-mx-3 flex flex-wrap items-center lg:justify-between">
+            <div class="mt-0 mb-6 w-full max-w-full shrink-0 px-3 lg:mb-0 lg:w-1/2 lg:flex-none">
+              <div class="text-center text-sm leading-normal text-slate-500 lg:text-left">
+                ©
+                <script>
+                  document.write(new Date().getFullYear() + ",");
+                </script>
+                made with <i class="fa fa-heart"></i> by
+                <a href="https://www.creative-tim.com" class="font-semibold text-slate-700 dark:text-white"
+                  target="_blank">Creative Tim</a>
+                for a better web.
+              </div>
+            </div>
+            <div class="mt-0 w-full max-w-full shrink-0 px-3 lg:w-1/2 lg:flex-none">
+              <ul class="mb-0 flex list-none flex-wrap justify-center pl-0 lg:justify-end">
+                <li class="nav-item">
+                  <a href="https://www.creative-tim.com"
+                    class="block px-4 pt-0 pb-1 text-sm font-normal text-slate-500 transition-colors ease-in-out"
+                    target="_blank">Creative Tim</a>
+                </li>
+                <li class="nav-item">
+                  <a href="https://www.creative-tim.com/presentation"
+                    class="block px-4 pt-0 pb-1 text-sm font-normal text-slate-500 transition-colors ease-in-out"
+                    target="_blank">About Us</a>
+                </li>
+                <li class="nav-item">
+                  <a href="https://creative-tim.com/blog"
+                    class="block px-4 pt-0 pb-1 text-sm font-normal text-slate-500 transition-colors ease-in-out"
+                    target="_blank">Blog</a>
+                </li>
+                <li class="nav-item">
+                  <a href="https://www.creative-tim.com/license"
+                    class="block px-4 pt-0 pb-1 pr-0 text-sm font-normal text-slate-500 transition-colors ease-in-out"
+                    target="_blank">License</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer> -->
+  </div>
+  <!-- end cards -->
+</x-admin-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,15 +15,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
+Route::prefix('admin')->name('admin.')->middleware([
+	'auth:sanctum',
+	config('jetstream.auth_session'),
+	'verified'
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+	Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 });
